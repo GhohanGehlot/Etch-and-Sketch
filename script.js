90861b2bbd403c91c33ca2d6072a98611636b792
Your Name <you@example.com>
2025-07-21 20:43:16 +0530

    random rgb genrator completed

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
-function handleGridSize(gridSize){
 
+ 
+ 
+ function handleGridSize(gridSize){
+    
     let gridSizeSquare = gridSize * gridSize;
 
     for(let i = 0 ; gridSizeSquare > i ; i++ ){
-        const div = document.createElement("div");
+       const div = document.createElement("div");
       const container = document.getElementById("container");
       container.appendChild(div);
       container.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`
@@ -13,11 +16,19 @@ function handleGridSize(gridSize){
    
 }
 
+function rgbRandomColor(){
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+    const randomColor = `rgb( ${r} , ${g} , ${b})`;
+    return randomColor;
+}
 
 
 
 document.addEventListener("DOMContentLoaded" , function exec() {
-
+    
+    
     let gridSize = prompt("Tell me the size of grid you want (not more than 20)");
 
         
@@ -29,9 +40,14 @@ document.addEventListener("DOMContentLoaded" , function exec() {
         handleGridSize(gridSize);
     }
 
+        const divs = document.querySelectorAll("#container div");
+
+        divs.forEach((div) => {
+            div.addEventListener("mouseenter" , function () {
+                this.style.backgroundColor = rgbRandomColor();
+            })
+        })
+
+
 })
-
-
-
-;
 
