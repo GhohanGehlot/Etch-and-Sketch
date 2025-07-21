db843b699f8bcd367b34926407a758c733ff93fb
Your Name <you@example.com>
2025-07-21 20:00:30 +0530

    Grid creation system done

--- script.js
+++ script.js
@@ -1,2 +1,37 @@
-const heading = document.getElementsByTagName("h1");
+function handleGridSize(gridSize){
+
+    let gridSizeSquare = gridSize * gridSize;
+
+    for(let i = 0 ; gridSizeSquare > i ; i++ ){
+        const div = document.createElement("div");
+      const container = document.getElementById("container");
+      container.appendChild(div);
+      container.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`
+
+
+    }
+   
+}
+
+
+
+
+document.addEventListener("DOMContentLoaded" , function exec() {
+
+    let gridSize = prompt("Tell me the size of grid you want (not more than 20)");
+
+        
+    if( gridSize > 20 || gridSize <= 0){
+        alert("Please enter correct size !!");
+        exec();
+
+    }else{
+        handleGridSize(gridSize);
+    }
+
+})
+
+
+
+;
 
